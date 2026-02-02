6befa536df842f7497c9960c7acd935cf6c84f28
Rosen Filipov <rfilipov010@gmail.com>
2026-02-02 20:50:08 +0200

    update chromeoptions arguments

--- SeleniumIDE/TC01IfUserIsInvalidTryAgainTest1.cs
+++ SeleniumIDE/TC01IfUserIsInvalidTryAgainTest1.cs
@@ -22,11 +22,7 @@ public class TC01IfUserIsInvalidTryAgainTest
     public void SetUp()
     {
         ChromeOptions options = new ChromeOptions();
-        options.AddArguments("headless");
-        options.AddArguments("no-sandbox");
-        options.AddArguments("disable-dev-shm-usage");
-        options.AddArguments("disable-gpu");
-        options.AddArguments("window-size=1920x1080");
+        options.AddArguments("--headless-new");
 
         driver = new ChromeDriver();
         js = (IJavaScriptExecutor)driver;
